a20d94621bf6a174924a343dc99f06b0ebf89bf4
unknown <anshpatel6781@gmail.com>
2025-05-19 15:02:56 +0530

    React JS

---
+++ React/my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Hello from "./Jsx/Hello";
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>This is React</h1>
+
+
+      {/* JSX */}
+       < Hello />  
+    
+    </div>
+  )
+}
+
+export default App
---
+++ React/my-react-app/src/JSX/Hello.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default
+
+function Hello() {
+
+   
+
+   
+
+let person={
+    name:"Raju",
+    course:"Full-stack",
+}
+
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel numquam ipsa unde eum,
+                eos fugiat nesciunt. Odio, repellat illo voluptates esse
+                vitae ipsum quo porro dolorem perspiciatis non suscipit. Iste.</h1>
+            <h3>Name {person.name}</h3>
+            <h4>Course {person.course}</h4>
+            
+        </div>
+    )
+}
